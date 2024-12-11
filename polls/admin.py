56a16bcdbcbd9e932ac0cd307ca921926dff0b5f
hprasad <hrishikeshprasadc@gmail.com>
2024-12-11 08:02:28 +0530

    poll app finished

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import *
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question", {
+            "fields": ['question_text']
+        }),("Date information",{"fields":['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_display =[ 'question_text' , 'pub_date' ,'was_published_recently']
+    
+admin.site.register(Question,QuestionAdmin)
+# admin.site.register(Choice)
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import datetime
+from .models import Question
+from django.utils import timezone
 
 # Create your tests here.
+
+
+class QuestionModeTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() function must returns false for questions in the future
+        """
+        future_date = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date = future_date)
+        self.assertIs(future_question.was_published_recently(),False)
